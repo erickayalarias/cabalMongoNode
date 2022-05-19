d8294e8f47583793061a2b18f97f8581a3170b08
Erick Ayala <erickayalrias@gmail.com>
2022-05-19 11:24:19 +0200

    master

--- src/models/User-model.js
+++ src/models/User-model.js
@@ -2,89 +2,91 @@ const mongoose = require("mongoose");
 const validator = require("validator");
 
 const UserSchema = new mongoose.Schema(
-  {
-    publicKey: {
-      type: String,
+    {
+        publicKey: {
+            type: String,
+        },
+        uid: {
+            type: String,
+        },
+        username: {
+            type: String,
+            unique: true,
+        },
+        avatar: {
+            type: String,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        communities: [
+            {
+                type: mongoose.Types.ObjectId,
+                ref: "communities",
+            },
+        ],
+        playlist: [
+            {
+                type: mongoose.Types.ObjectId,
+                ref: "playlist",
+            },
+        ],
+        music: [
+            {
+                type: mongoose.Types.ObjectId,
+                ref: "music",
+            },
+        ],
+        friends: [
+            {
+                user: {
+                    type: mongoose.Types.ObjectId,
+                    ref: "users",
+                },
+                password: {
+                    type: String,
+                },
+            },
+        ],
+        pendingfriends: [
+            {
+                type: mongoose.Types.ObjectId,
+                ref: "friendEnvitation",
+            },
+        ],
+        pendingcommunities: [
+            {
+                type: mongoose.Types.ObjectId,
+                ref: "communityInvitation",
+            },
+        ],
+        videos: [
+            {
+                type: mongoose.Types.ObjectId,
+                ref: "videos",
+            },
+        ],
+        images: [
+            {
+                type: Number,
+                default: [0, 0, 0],
+            },
+        ],
+        chat: [
+            {
+                type: mongoose.Types.ObjectId,
+                ref: "chat",
+            },
+        ],
+        posts: [
+            {
+                type: mongoose.Types.ObjectId,
+                ref: "posts",
+            },
+        ],
     },
-    uid: {
-      type: String,
-    },
-    username: {
-      type: String,
-      unique: true
-    },
-    avatar: {
-      type: String,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
-    communities: [
-      {
-        type: mongoose.Types.ObjectId,
-        ref: "communities",
-      },
-    ],
-    playlist: [
-      {
-        type: mongoose.Types.ObjectId,
-        ref: "playlist",
-      },
-    ],
-    music: [
-      {
-        type: mongoose.Types.ObjectId,
-        ref: "music",
-      },
-    ],
-    friends: [{
-      user: {
-        type: mongoose.Types.ObjectId,
-        ref: "users",
-      },
-      password: {
-        type: String
-      }
-    }],
-    pendingfriends: [
-      {
-        type: mongoose.Types.ObjectId,
-        ref: "friendEnvitation",
-      },
-    ],
-    pendingcommunities: [
-      {
-        type: mongoose.Types.ObjectId,
-        ref: "communityInvitation",
-      },
-    ],
-    videos: [
-      {
-        type: mongoose.Types.ObjectId,
-        ref: "videos",
-      },
-    ],
-    images: [
-      {
-        type: Number
-      },
-    ],
-    chat: [
-      {
-        type: mongoose.Types.ObjectId,
-        ref: "chat",
-      },
-    ],
-    posts: [
-      {
-        type: mongoose.Types.ObjectId,
-        ref: "posts",
-      },
-    ],
-  },
-  { timestamps: true, versionKey: false }
+    { timestamps: true, versionKey: false }
 );
-
 
 module.exports = mongoose.model("users", UserSchema);
